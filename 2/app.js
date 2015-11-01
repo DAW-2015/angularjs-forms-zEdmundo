5614e93f4b63f2e944a0ff74f325ab8b27cfcec9
zEdmundo <edmundo-rodrigues@hotmail.com.br>
2015-11-01 11:41:28 -0200

    second exercise

---
+++ 2/app.js
@@ -0,0 +1,61 @@
+(function () {
+  "use strict";
+
+  var app = angular.module("myApp", []);
+
+  app.controller("formController", ["$scope", function ($scope) {
+    $scope.user = {};
+    $scope.master = {};
+
+    this.submit = function () {
+      angular.copy($scope.user, $scope.master);
+    };
+
+    this.reset = function () {
+      $scope.user = {};
+    };
+  }]);
+
+  app.directive("cpfValidator", function () {
+    var cpfValidator = (function () {
+      return {
+        require: "ngModel",
+        link: function (scope, elm , attrs, ctrl) {
+          ctrl.$validators.cpfValidator = function (modelValue, viewValue) {
+            var the_cpf = viewValue;
+
+            if (viewValue === undefined || the_cpf.length != 11)
+              return false;
+
+            var forCalc = the_cpf.substring(0, 9), forValidate = the_cpf.substring(9, 11);
+
+            function calcDigit(_from) {
+                // first step
+                var sum = 0;
+
+                for (var fat = _from; fat >= 2; fat--)
+                  sum += parseInt(the_cpf[_from - fat])*fat;
+
+                // second step
+                var digit = (sum*10)%11;
+
+                if (digit == 10)
+                  digit = 0;
+
+                return digit;
+            }
+
+            /* validate */
+            if (calcDigit(10) != parseInt(forValidate[0]) || calcDigit(11) != parseInt(forValidate[1]))
+              return false;
+
+            return true;
+          }
+        }
+      }
+    })();
+
+    return cpfValidator;
+  });
+
+})();
